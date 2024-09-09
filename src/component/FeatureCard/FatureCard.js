9e7577e9d1d68fb7cb6b2c477794569a1662afae
Adityagaurkar09 <adityagaurkar09@gmail.com>
2024-09-09 16:19:43 +0530

    landing page created

---
+++ src/component/FeatureCard/FatureCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FeatureCard.css'
+
+function FatureCard({FeatureImg,FeatureText}) {
+  return (
+    <div className='feature-card '>
+    <img src={FeatureImg} className='feature-img' alt='secure'></img>
+    <p className='feature-text text-center '>{FeatureText}</p>
+   </div>
+  )
+}
+
+export default FatureCard
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,17 +1,60 @@
 import React from 'react'
 import './Home.css'
+import imgSecure from "./secure.png"
+import imgBrain from "./brain.png"
+import heartImg from "./heart.png"
+import FatureCard from '../../component/FeatureCard/FatureCard'
+
+const FEATURE = [
+  {
+     FeatureImg:imgSecure,
+      FeatureText :"Note Will Be Securly Stored In Local Browswe Storage"
+  },
+  {
+    FeatureImg:imgBrain,
+     FeatureText :"Use Your Brain For THinking , Not For Remembering Thing"
+ },
+ {
+  FeatureImg:heartImg,
+   FeatureText :" Loved By 100+ User. Feedback Are Always Welcome !"
+}
+]
 
 function Home() {
   return (
     <div>
- <h1 className='text-center text-primary'>Quick-Note</h1>
+ <h1 className='text-center text-primary heading'>Quick-Note</h1>
+ 
+<h2>
  <p className='text-center text-secondary'>Your Pcket Friendly Toking App.
   <del>Never Forget Anyting Again !</del>
   <span className='text-heilight text-primary'> Remember Everything !</span>
  </p>
+ </h2>
+
+ <div>
+
+ <h2 className='text-center '>Why Should You Use Quick Note ? </h2>
+
+ <div className='feature-container'>
+
+{
+  FEATURE.map((feature)=>{
+    const {FeatureImg,FeatureText}=feature;
+    return(
+    <FatureCard
+    FeatureImg={FeatureImg}
+    FeatureText={FeatureText}
+    />
+  );
+  })
+}
+ </div>
+
+ </div>
  <div className='button-container text-center'>
-  <button  className='btn-primary'>ADD NOTE</button>
-  <button  className='btn-primary'>SHOW NOTE</button>
+  <button  className='btn-primary purple-color  bold-text'>ADD NOTE</button>
+  <button  className='btn-primary text-heilight bold-text '>SHOW NOTE</button>
  </div>
  </div>
   )
